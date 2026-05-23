b890bb3a21d1458196f7601dab535bbe1c891365
opfez <opfez@disroot.org>
2026-05-23 19:08:27 +0000

    fix spelling in docstring of olivetti-body-width

--- olivetti.el
+++ olivetti.el
@@ -175,7 +175,7 @@ If an integer, set text body width to that integer in columns; if
 a floating point between 0.0 and 1.0, set text body width to that
 fraction of the total window width. If nil (the default), use the
 value of `fill-column' + 2. The extra 2 columns are to prevent
-text files at `fill-colum ' from wrapping in the body widith.
+text files at `fill-column' from wrapping in the body width.
 
 An integer is best if you want text body width to remain
 constant, while a floating point is best if you want text body
